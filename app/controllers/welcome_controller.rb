07b256c0fcf8121822d651b824dd75a8a8d11eaa
brayzen <brian.ray.biz@gmail.com>
2016-01-08 12:52:49 -0800

    logging response

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,16 +4,16 @@ class WelcomeController < ApplicationController
 
   def index
     puts "///////////////////////////////////////////////////"
-    puts "query__params: #{query_params}"
+    puts "queryz____params: #{query_params}"
     puts "$$$$$$$$$$$$"
     if query_params
+      puts '%%%%%%%%%%%%%%%%%%%%%%%%%%'
       format_dates
+      puts '%%%%%%%%%%%%%%%%%%%%%%%%%%'
       queery = query_params[:queery]
       limit = query_params[:limit]
       response = HTTParty.get("http://api.boardreader.com/v1/Boards/Search?&offset=0&limit=#{limit}&query=#{queery}&group_mode=post&filter_date_from=#{@date_from}&filter_date_to=#{@date_to}&sort_mode=default&filter_language=&dn=&body=snippet&mode=full&match_mode=extended&key=#{Rails.application.secrets.wy_api_key}")
-      puts '%%%%%%%%%%%%%%%%%%%%%%%%%%'
-      puts '%%%%%%%%%%%%%%%%%%%%%%%%%%'
-      puts response["Response"]["Request"]["key"]
+      puts "***#{response["Response"]["Request"]["key"]}"
       puts '%%%%%%%%%%%%%%%%%%%%%%%%%%'
       puts '%%%%%%%%%%%%%%%%%%%%%%%%%%'
       if response["Response"]["Matches"]
@@ -34,10 +34,10 @@ class WelcomeController < ApplicationController
   def format_dates
     @date_from = Date.new(query_params[:"date_from(1i)"].to_i, query_params[:"date_from(2i)"].to_i, query_params[:"date_from(3i)"].to_i).to_time.to_i
     @date_to = Date.new(query_params[:"date_to(1i)"].to_i, query_params[:"date_to(2i)"].to_i, query_params[:"date_to(3i)"].to_i).to_time.to_i
-    puts "in qParams function:::::::::::::::"
-    puts "QUERY Object: #{query_params[:"date_to(1i)"]}"
-    puts "!~~~~~~~~#{@date_to}~~~~~~~~!"
-    puts "in qParams function^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+    # puts "in qParams function:::::::::::::::"
+    # puts "QUERY Object: #{query_params[:"date_to(1i)"]}"
+    # puts "!~~~~~~~~#{@date_to}~~~~~~~~!"
+    # puts "in qParams function^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
   end
 
   def query_params
