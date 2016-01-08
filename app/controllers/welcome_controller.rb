8b7a4563b19bc941fbcded9af433e9f6ca2eb441
brayzen <brian.ray.biz@gmail.com>
2016-01-08 15:05:51 -0800

    added siteTitle as the link to the URL

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,18 +3,14 @@ class WelcomeController < ApplicationController
   require 'time'
 
   def index
-    puts "///////////////////////////////////////////////////"
-    puts "queryz____params: #{query_params}"
-    puts "$$$$$$$$$$$$"
     if query_params
       format_dates
       queery = query_params[:queery].gsub(' ', '+')
-      limit = query_params[:limit]
-      puts "*******  #{queery}"
+      limit = query_params[:limit] || '100'
       response = HTTParty.get("http://api.boardreader.com/v1/Boards/Search?&offset=0&limit=#{limit}&query=#{queery}&group_mode=post&filter_date_from=#{@date_from}&filter_date_to=#{@date_to}&sort_mode=default&filter_language=&dn=&body=snippet&mode=full&match_mode=extended&key=#{Rails.application.secrets.wy_api_key}")
       if response["Response"]["Matches"]
         matches = response["Response"]["Matches"]["Match"]
-        winners = ["Country", "Language", "Crawled", "Url", "Text", "Subject", "Published"]
+        winners = ["SiteTitle", "Country", "Language", "Crawled", "Text", "Subject", "Published"]
         @keys = matches[0].keys.select{ |winner| winners.include?(winner) }
         @matches = matches.map { |match| match }
       else
@@ -30,10 +26,6 @@ class WelcomeController < ApplicationController
   def format_dates
     @date_from = Date.new(query_params[:"date_from(1i)"].to_i, query_params[:"date_from(2i)"].to_i, query_params[:"date_from(3i)"].to_i).to_time.to_i
     @date_to = Date.new(query_params[:"date_to(1i)"].to_i, query_params[:"date_to(2i)"].to_i, query_params[:"date_to(3i)"].to_i).to_time.to_i
-    # puts "in qParams function:::::::::::::::"
-    # puts "QUERY Object: #{query_params[:"date_to(1i)"]}"
-    # puts "!~~~~~~~~#{@date_to}~~~~~~~~!"
-    # puts "in qParams function^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
   end
 
   def query_params
